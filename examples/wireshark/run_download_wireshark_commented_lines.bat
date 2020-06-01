9f3fbb6e425b3593f4faef67065d2519c4de1941
Miroslaw Staron <miroslaw.staron@gu.se>
2020-06-01 14:00:51 +0200

    Fixed merge conflicts

--- examples/wireshark/run_download_wireshark_commented_lines.bat
+++ examples/wireshark/run_download_wireshark_commented_lines.bat
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-python ../../scripts/download_commented_lines_from_gerrit.py "https://code.wireshark.org/review/" "data\wireshark_commented_lines_merged.csv" "/changes/?q=status:merged&o=ALL_FILES&o=ALL_REVISIONS&o=DETAILED_LABELS"  --sleep_between_pages 1 --n 10 --max_queries 5 --max_fails 100 --sep "$" --from_date "2020-05-01" --to_date "2020-05-30"
-=======
 python ../../scripts/download_commented_lines_from_gerrit.py "https://code.wireshark.org/review/" ^
      "data\wireshark_commented_lines_merged.csv" ^
      "/changes/?q=status:merged&o=ALL_FILES&o=ALL_REVISIONS&o=DETAILED_LABELS" ^
@@ -11,6 +8,3 @@ python ../../scripts/download_commented_lines_from_gerrit.py "https://code.wires
      --sep "$" ^
      --from_date "2015-02-23" ^
      --to_date "2020-05-30"
->>>>>>> 462351b5db2782de34120624dc93491b705f3a84
-
-
